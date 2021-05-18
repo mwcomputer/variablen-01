40b9df9c720870fbc8c78edeff207b154fd0c5e5
mwcomputer <kolbenfresser5@googlemail.com>
2021-05-18 14:55:15 +0200

    stundenende

--- script_01.js
+++ script_01.js
@@ -60,7 +60,7 @@ console.log("birthYearMark: " + birthYearMark);
 */
 
 /***** Variablen 03 *******/
-
+/*
 let ageJohn, ageMark;
 ageJohn = 25;
 ageMark = 30;
@@ -68,10 +68,20 @@ ageMark = 30;
 // Opperation (Vergleich/Test)
 let isJohnOlder = (ageJohn > ageMark);
 console.log(isJohnOlder);
+*/
 
+/***** Variablen 04 *******/
+// Zusammenfassung Datentypen
 
+let nix;
+let ageJohn = 25;
+firstName = "Max";
+let isJohnOlder = (10 > 5);
 
-
+console.log("nix: " + typeof nix);
+console.log("ageJohn: " + typeof ageJohn);
+console.log("firtsName: " + typeof firstName);
+console.log("isJohnOlder: " + typeof isJohnOlder); 
 
 
 
